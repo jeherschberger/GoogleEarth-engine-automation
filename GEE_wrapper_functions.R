4fa098eaa1eafaa1c389191913e709ce60267a57
Jacob Herschberger <jakeberger92@gmail.com>
2026-08-04 11:34:10 +0700

    Add function to extract bands and dates from GEE
    This function extracts multiple bands and dates from Google Earth Engine, processing locations in batches and handling both image and image collection selections.

---
+++ GEE_wrapper_functions.R
@@ -0,0 +1,114 @@
+## Extract multiple bands and dates from google earth engine----
+Extract_var_with_const_date <- function(loc,path,subpath,select,
+                                        begin = 5,
+                                        end=7,
+                                        unit="month",
+                                        begin1 = 5,
+                                        end1 =7,
+                                        unit1 = "",
+                                        buffer=1500,
+                                        scale=300) {
+  batch_size=round(sqrt(6000/(3.14*(buffer/scale)^2)))
+  print(batch_size)
+
+  if(select!=T){
+    terra<-ee$Image(as.character(path))
+    n_images <- 1
+
+
+  } else if(select==T){
+    terra<-ee$ImageCollection(as.character(path))$select(as.character(subpath))
+
+
+    if(nchar(unit)>1) {
+      terra<-terra$filter(
+        ee$Filter$calendarRange(
+          begin,
+          end,
+          unit
+        )
+      )
+    }
+
+    if(nchar(unit1)>1) {
+      terra<-terra$filter(
+        ee$Filter$calendarRange(
+          begin1,
+          end1,
+          unit1
+        )
+      )
+    }
+
+
+  n_images <- terra$size()$getInfo()
+}
+
+  num_iterations <- ceiling(nrow(loc) / batch_size)
+
+  results<-list()
+  # Loop through each batch
+  for (i in 1:num_iterations) {
+    # Calculate the start and end rows for the current batch
+    start_row <- (i - 1) * batch_size + 1
+    end_row <- min(i * batch_size, nrow(loc))
+    print(paste("Processing locations", start_row,"-",end_row, "of", nrow(loc)))
+    # Extract current batch
+    batch <- sf_as_ee(loc[start_row:end_row, ])$map(function(pt) {                         # pt is an ee.Feature
+      pt$buffer(buffer)
+    })
+
+
+    im_iter <- ceiling(n_images / batch_size)
+
+
+    if(select!=T) {
+      row<-terra$sampleRegions(
+        collection = batch,
+        scale = scale,
+        geometries = FALSE
+      ) %>%
+        ee_as_sf() %>% sf::st_drop_geometry()
+
+      results[[paste(i)]]<-row
+
+
+    } else {
+    for (j in 1:im_iter) {
+      start_im <- (j - 1) * batch_size + 1
+      end_im <- min(j * batch_size, n_images)
+
+      print(paste("Processing images", start_im,"-",end_im, "of", n_images))
+
+      # Get the i-th image (0-indexed in EE)
+      img <- ee$ImageCollection$fromImages(terra$toList(n_images)$slice(start_im,end_im))
+
+    # Perform ee_extract for the current batch
+    row <-img$map(
+      ee_utils_pyfunc(function(img) {
+        img_date <- img$date()$format("YYYY-MM-dd")
+
+        sampled <- img$sampleRegions(
+          collection = batch,
+          scale = scale,
+          geometries = FALSE
+        )
+        sampled$map(
+          ee_utils_pyfunc(function(feature) {
+            feature$set("image_date", img_date)
+          })
+        )
+      })
+     )$flatten() %>% ee_as_sf() %>% sf::st_drop_geometry()
+    results[[paste(i,j)]]<-row
+    }
+
+    }
+
+  }
+
+  results<-do.call(rbind, results)
+
+
+  results
+}
