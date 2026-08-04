343b25541cdfc2784f034a5b3a9cba539ca933cf
Jacob Herschberger <jakeberger92@gmail.com>
2026-08-04 13:46:21 +0700

    Update authentication process for Earth Engine
    Set environment variable for Google application credentials and clean user credentials.

--- get_data.R
+++ get_data.R
@@ -1,8 +1,13 @@
-library(rgee)
 library(jsonlite)
 
 # 1. Authenticate Earth Engine using the secret token string stored on github secrets ----
-creds_json <- Sys.getenv("EARTHENGINE_TOKEN")
+creds_json <- Sys.setenv(
+  GOOGLE_APPLICATION_CREDENTIALS = normalizePath("sa_key.json")
+)
+
+library(rgee)
+
+ee_clean_user_credentials()
 
 if (creds_json == "") {
   print("Please add GoogleEarth Engine secret key to repository!!")
